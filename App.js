1aa4855bd6537b53cbd4483e5b0d9394ccb6890e
SamPatt <git@sampatt.com>
2024-01-25 14:38:23 -0500

    Revert "New folder structure; stubbed out services"
    This reverts commit 40aa93956d8620bdf9a5ae25437b703b2267ca09.

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>LookMa AI</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: '#757575',
+  },
+});
